2a4adb1c65d901318d0c472a9dfe9fb02beb164f
Marcin Mazurkiewicz <marcin.mazurkiewicz.ac@gmail.com>
2017-02-08 13:42:27 +0100

    CPU done with some help from github (I didn't feel the need to work on it)

--- hack/hackassembly/fill/Fill.asm
+++ hack/hackassembly/fill/Fill.asm
@@ -1,57 +1,102 @@
-// This file is part of www.nand2tetris.org
-// and the book "The Elements of Computing Systems"
-// by Nisan and Schocken, MIT Press.
-// File name: projects/04/Fill.asm
-
-// Runs an infinite loop that listens to the keyboard input. 
+// Runs an infinite loop that listens to the keyboard input.
 // When a key is pressed (any key), the program blackens the screen,
-// i.e. writes "black" in every pixel. When no key is pressed, the
-// program clears the screen, i.e. writes "white" in every pixel.
+// i.e. writes "black" in every pixel;
+// the screen should remain fully black as long as the key is pressed. 
+// When no key is pressed, the program clears the screen, i.e. writes
+// "white" in every pixel;
+// the screen should remain fully clear as long as no key is pressed.
 
-    @status
-    M=-1        // status=0xFFFF
-    D=0         // Argument - what to set screen bits to
-    @SETSCREEN
-    0;JMP
+// Screen is 256 by 512 and starts at @SCREEN
 
-(LOOP)
-    @KBD
-    D=M         // D = current keyboard character
-    @SETSCREEN
-    D;JEQ       // If no key, set screen to zeroes (white)
-    D=-1        // If key pressed, set screen to all 1 bits (black)
-    
-(SETSCREEN)     // Set D=new status before jumping here
-    @ARG
-    M=D         // Save new status arg
-    @status     // FFFF=black, 0=white - status of entire screen
-    D=D-M       // D=newstatus-status
-    @LOOP
-    D;JEQ        // Do nothing if new status == old status
-    
-    @ARG
-    D=M
-    @status
-    M=D         // status = ARG
-    
-    @SCREEN
-    D=A         // D=Screen address
-    @8192
-    D=D+A       // D=Byte just past last screen address
-    @i
-    M=D         // i=SCREEN address
-    
-(SETLOOP)    
-    @i
-    D=M-1
-    M=D         // i=i-1
-    @LOOP
-    D;JLT       // if i<0 goto LOOP
-    
-    @status
-    D=M         // D=status
-    @i
-    A=M         // Indirect
-    M=D         // M[current screen address]=status
-    @SETLOOP
-    0;JMP
+// Variable initialization
+@8192
+D=A
+@size // set the range of screen restricted memory addresses ((512*256)/16), divided by 16 because each memory location contains 16 pixels
+M=D
+
+//@i
+//M=0
+//@COLOR_BLACK
+//0; JEQ
+
+@state
+M=0
+
+@IDLE
+0; JEQ
+
+(IDLE)
+   @KBD
+   D=M
+
+   // If no key pressed then go to color screen directly
+   @COLOR_SCREEN
+   D; JEQ
+
+   // If key pressed then change state
+   D=1
+   @COLOR_SCREEN
+   0; JEQ
+
+(COLOR_SCREEN)
+   @state
+   M=D-M // set state comparison value 0 if nothing changed
+   D=M
+
+   @IDLE // if state comparison == 0 then go back to loop
+   D; JEQ
+
+   @i // reset loop counter before executing any loops
+   M=0
+   
+   @COLOR_BLACK
+   D; JGT // state changes to 1 thus we need to render black screen
+
+   @state // button was freed thus we need to white-color the screen and reset the status
+   M=0
+   @COLOR_WHITE
+   D; JLT
+
+(COLOR_BLACK)
+   @i // get iterator
+   D=M
+   @SCREEN // get screen start in memory and shift the pointer by iterator
+   A=A+D
+   M=-1 // set values to 0xffff (black)
+
+   @i // increase iterator
+   D=M+1
+   M=D
+
+   @size // get size and calculate difference between size and current iterator
+   D=M-D
+
+   @IDLE // if difference < 0 then go back to idle because rendering is finished
+   D; JLE
+
+   @COLOR_BLACK // else continue the loop because there are still some pixels to color
+   0; JEQ
+
+
+(COLOR_WHITE)
+   @i // get iterator
+   D=M
+   @SCREEN // get screen start in memory and shift the pointer by iterator
+   A=A+D
+   M=0 // set values to 0x0000 (white)
+
+   @i // increase iterator
+   D=M+1
+   M=D
+
+   @size // get size and calculate difference between size and current iterator
+   D=M-D
+
+   @IDLE // if difference < 0 then go back to idle because rendering is finished
+   D; JLE
+
+   @COLOR_WHITE // else continue the loop because there are still some pixels to color
+   0; JEQ
+
+
+
---
+++ hack/hackassembly/readme.txt
@@ -0,0 +1,4 @@
+This containes programs written in Hack Assembly, a language that is provided for Hack CPU by the course.
+   You can run those using provided CPU emulator (it automatically compiles it into machine code).
+   - Mult: Simple multiplication program, it multiples value from RAM[0] by RAM[1] and saves the outcome to RAM[2]
+   - Fill: Pressing and holding any key should make the screen black, freeing the key should make it white again.
